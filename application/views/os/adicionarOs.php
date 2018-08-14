4c306eb3bf0499c55143efc4642e95ea4b938074
NeftaliDiaz <neftali.dico@gmail.com>
2018-08-14 13:31:11 -0500

    make it better

---
+++ application/views/os/adicionarOs.php
@@ -0,0 +1,178 @@
+<link rel="stylesheet" href="<?php echo base_url();?>js/jquery-ui/css/smoothness/jquery-ui-1.9.2.custom.css" />
+<script type="text/javascript" src="<?php echo base_url()?>js/jquery-ui/js/jquery-ui-1.9.2.custom.js"></script>
+<script type="text/javascript" src="<?php echo base_url()?>js/jquery.validate.js"></script>
+<div class="row-fluid" style="margin-top:0">
+    <div class="span12">
+        <div class="widget-box">
+            <div class="widget-title">
+                <span class="icon">
+                    <i class="icon-tags"></i>
+                </span>
+                <h5>Registrar Requisición</h5>
+            </div>
+            <div class="widget-content nopadding">
+                
+
+                <div class="span12" id="divProdutosServicos" style=" margin-left: 0">
+                    <ul class="nav nav-tabs">
+                        <li class="active" id="tabDetalhes"><a href="#tab1" data-toggle="tab">Detalles de la Requisición</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab1">
+
+                            <div class="span12" id="divCadastrarOs">
+                                <?php if($custom_error == true){ ?>
+                                <div class="span12 alert alert-danger" id="divInfo" style="padding: 1%;">Datos incompletos, comprobar los campos con un asterisco o PERSONAL correctamente seleccionado y responsable.</div>
+                                <?php } ?>
+                                <form action="<?php echo current_url(); ?>" method="post" id="formOs">
+
+                                    <div class="span12" style="padding: 1%">
+                                        <div class="span6">
+                                            <label for="cliente">Personal<span class="required">*</span></label>
+                                            <input id="cliente" class="span12" type="text" name="cliente" value=""  />
+                                            <input id="clientes_id" class="span12" type="hidden" name="clientes_id" value=""  />
+                                        </div>
+                                        <div class="span6">
+                                            <label for="tecnico">Responsble<span class="required">*</span></label>
+                                            <input id="tecnico" class="span12" type="text" name="tecnico" value=""  />
+                                            <input id="usuarios_id" class="span12" type="hidden" name="usuarios_id" value=""  />
+                                        </div>
+                                    </div>
+                                    <div class="span12" style="padding: 1%; margin-left: 0">
+                                        <div class="span3">
+                                            <label for="status">Estado<span class="required">*</span></label>
+                                           <select class="span12" name="status" id="status" value="">
+                                                <option value="Cancelada">Cancelada</option>
+                                                <option value="Colocada">Colocada</option>
+                                                <option value="Cotizada">Cotizada</option>
+                                                <option value="En proceso de Autorización">En proceso de Autorización</option>
+                                                <option value="No autorizada">No autorizada</option>
+                                                <option value="Pendiente de Pago">Pendiente de Pago</option>
+                                                <option value="Revisión">Revisión</option>
+                                            </select>
+                                        </div>
+                                        <div class="span3">
+                                            <label for="dataInicial">Fecha Inicial<span class="required">*</span></label>
+                                            <input id="dataInicial" class="span12 datepicker" type="text" name="dataInicial" value=""  />
+                                        </div>
+                                        <div class="span3">
+                                            <label for="dataFinal">Fecha Final</label>
+                                            <input id="dataFinal" class="span12 datepicker" type="text" name="dataFinal" value=""  />
+                                        </div>
+
+                                        <div class="span3">
+                                            <label for="garantia">Garantía</label>
+                                            <input id="garantia" type="text" class="span12" name="garantia" value=""  />
+                                        </div>
+                                    </div>
+
+                                    <div class="span12" style="padding: 1%; margin-left: 0">
+
+                                        <div class="span6">
+                                            <label for="descricaoProduto">Descripción Produto/Servicio</label>
+                                            <textarea class="span12" name="descricaoProduto" id="descricaoProduto" cols="30" rows="5">PRODUCTO: 
+ *  MARCA: 
+ *  MODELO:  
+ *  S/Nº: 
+ </textarea>
+                                                                                  
+                                        </div>
+                                        <div class="span6">
+                                            <label for="defeito">Defecto</label>
+                                            <textarea class="span12" name="defeito" id="defeito" cols="30" rows="5"></textarea>
+                                        </div>
+
+                                    </div>
+                                    <div class="span12" style="padding: 1%; margin-left: 0">
+                                        <div class="span6">
+                                            <label for="observacoes">Observaciones</label>
+                                            <textarea class="span12" name="observacoes" id="observacoes" cols="30" rows="5"></textarea>
+                                        </div>
+                                        <div class="span6">
+                                            <label for="laudoTecnico">Diagnóstico o</label>
+                                            <textarea class="span12" name="laudoTecnico" id="laudoTecnico" cols="30" rows="5"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="span12" style="padding: 1%; margin-left: 0">
+                                        <div class="span6 offset3" style="text-align: center">
+                                            <button class="btn btn-success" id="btnContinuar"><i class="icon-share-alt icon-white"></i> Continuar</button>
+                                            <a href="<?php echo base_url() ?>index.php/os" class="btn"><i class="icon-arrow-left"></i> Volver</a>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                
+.
+             
+        </div>
+        
+    </div>
+</div>
+</div>
+
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+      $("#cliente").autocomplete({
+            source: "<?php echo base_url(); ?>index.php/os/autoCompleteCliente",
+            minLength: 1,
+            select: function( event, ui ) {
+
+                 $("#clientes_id").val(ui.item.id);
+                
+
+            }
+      });
+
+      $("#tecnico").autocomplete({
+            source: "<?php echo base_url(); ?>index.php/os/autoCompleteUsuario",
+            minLength: 1,
+            select: function( event, ui ) {
+
+                 $("#usuarios_id").val(ui.item.id);
+
+
+            }
+      });
+
+      
+      
+
+      $("#formOs").validate({
+          rules:{
+             cliente: {required:true},
+             tecnico: {required:true},
+             dataInicial: {required:true}
+          },
+          messages:{
+             cliente: {required: 'Campo Requerido.'},
+             tecnico: {required: 'Campo Requerido.'},
+             dataInicial: {required: 'Campo Requerido.'}
+          },
+
+            errorClass: "help-inline",
+            errorElement: "span",
+            highlight:function(element, errorClass, validClass) {
+                $(element).parents('.control-group').addClass('error');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).parents('.control-group').removeClass('error');
+                $(element).parents('.control-group').addClass('success');
+            }
+       });
+
+    $(".datepicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
+   
+});
+
+</script>
+
